32ce9633dacccc72e2d333152ecbc8031d892485
Igor <azawertik@gmail.com>
2022-09-13 01:04:09 +0300

    feat:: добавил новые эндпоинты для юзера и админа (получение всех пользователей, удаление, подсчет пользователей,редактирование профиля)

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,10 @@
-import { Body, Controller, Get, HttpCode, Param, Put, UsePipes, ValidationPipe } from "@nestjs/common";
+import { Body, Controller, Delete, Get, HttpCode, Param, Put, Query, UsePipes, ValidationPipe } from "@nestjs/common";
 import { UserService } from "./user.service";
 import { Auth } from "../auth/decorators/auth.decorator";
 import { User } from "./decorators/user.decorator";
 import { updateUserDto } from "./dto/updateUser.dto";
 import { idValidationPipe } from "../pipes/id.validation.pipe";
+import { UserModel } from "./user.model";
 
 
 @Controller('users')
@@ -32,5 +33,32 @@ export class UserController {
 
   async updateUser(@Param('id',idValidationPipe)id:string,@Body()dto:updateUserDto){
     return this.userService.updateUser(id,dto)
+  }
+
+
+  @Get('/count')
+  @Auth('admin')
+  async getCount(){
+    return this.userService.getCount()
+  }
+
+  @UsePipes(new ValidationPipe())
+  @Delete('/:id')
+  @Auth('admin')
+
+  async deleteUser(@Param('id',idValidationPipe)id:string){
+    return this.userService.deleteUser(id)
+  }
+  @UsePipes(new ValidationPipe())
+  @Get('/')
+  @Auth('admin')
+  async getAllUsers(@Query('searchTerm')searchTerm?:string){
+    return this.userService.getAllUsers(searchTerm)
+  }
+  @UsePipes(new ValidationPipe())
+  @Get('/:id')
+  @Auth('admin')
+  async getASingleUser(@Param('id',idValidationPipe)id:string){
+    return this.userService.getUserProfile(id)
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Controller, Injectable, NotFoundException } from "@nestjs/common";
+import { BadGatewayException, BadRequestException,  Injectable, NotFoundException } from "@nestjs/common";
 import { InjectModel } from "nestjs-typegoose";
 import { UserModel } from "./user.model";
 import { ModelType } from "@typegoose/typegoose/lib/types";
@@ -34,4 +34,36 @@ export class UserService {
     await user.save()
     return {message:'Информация изменена успешно!'}
   }
+  async getCount(){
+    return this.UserModel.find({}).count().exec()
+  }
+  async deleteUser(_id:string){
+    const deletedUser = await this.UserModel.findByIdAndDelete({_id}).exec()
+    if(!deletedUser){
+      throw new BadRequestException('Пользователь не найден')
+    }
+    return {message:'Удаление произошло успешно!'}
+  }
+  async getAllUsers(searchTerm?:string){
+    let options = {}
+    if(searchTerm)
+      options = {
+      $or: [
+        {
+          email: new RegExp(searchTerm,'i')
+        }
+      ]
+      }
+    return  this.UserModel.find({searchTerm}).select('-password -updatedAt -__v').sort({
+      createdAt: 'desc'
+    })
+
+  }
+  async getUserProfile(_id:string){
+    const findUser = await this.UserModel.findById({_id})
+    if(!findUser)
+      throw new NotFoundException('Пользователь не найден')
+    return findUser
+  }
+
 }
