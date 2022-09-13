46865df28cd965222ec67a026c27597091094264
Igor <azawertik@gmail.com>
2022-09-14 01:35:56 +0300

    feat:: добавил загрузку файлов на сервер

---
+++ src/file/file.controller.ts
@@ -0,0 +1,28 @@
+import {
+  Controller,
+  HttpCode,
+  Post,
+  Query,
+  UploadedFile,
+  UseInterceptors,
+  UsePipes,
+  ValidationPipe
+} from "@nestjs/common";
+import { FileService } from "./file.service";
+import { Auth } from "../auth/decorators/auth.decorator";
+import { FileInterceptor } from "@nestjs/platform-express";
+
+@Controller('file')
+export class FileController {
+  constructor(private readonly  fileService:FileService) {
+
+  }
+  @UsePipes(new ValidationPipe())
+  @Post()
+  @HttpCode(200)
+  @Auth('admin')
+  @UseInterceptors(FileInterceptor('image'))
+  async uploadFiles(@UploadedFile() file: Express.Multer.File, @Query('folder')folder?:string) {
+    return this.fileService.saveFiles([file],folder)
+  }
+}
